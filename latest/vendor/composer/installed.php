168361d150e67e5ab09a0598c588a9bccd1e08c4
Nilawan-chd <55489333+Nilawan-chd@users.noreply.github.com>
2023-11-12 00:55:00 +0700

    composer

---
+++ latest/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd96fa02b98083ecac3b9ed4cd0a7753f050c0934',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd96fa02b98083ecac3b9ed4cd0a7753f050c0934',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
